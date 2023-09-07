22d671b46fe05a45cc3826e20b958c02b6d75c71
andreypa3zl0pez <119307032+andreypa3zl0pez@users.noreply.github.com>
2023-09-07 08:31:59 -0500

    Add files via upload

---
+++ trabajadores 211111.js
@@ -0,0 +1,15 @@
+/*Elabora un algoritmo que calcule el sueldo promedio de un grupo de empleados y mostrar
+el nombre de los empleados que ganan más que el promedio.*/
+var n1 = parseInt(prompt('Digite la cantidad de trabajadores :'));
+var n2  = parseInt( prompt('Digite el sueldo fijo'))
+    
+
+    var divicion  = n1/n2;
+
+    console.log('el aproximado de cada trabajador es  : ' + divicion );
+
+
+
+
+
+    
