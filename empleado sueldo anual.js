f3f61f406925de8223dcd8a62c2386ade4c72d37
andreypa3zl0pez <119307032+andreypa3zl0pez@users.noreply.github.com>
2023-09-07 08:38:47 -0500

    .....
    2 algoritmo

---
+++ empleado sueldo anual.js
@@ -0,0 +1,6 @@
+var n1 = parseInt(prompt('el sueldo fijo del empleado  :'));
+var n2  = 12;
+
+    var multiplicacion   = n1*n2;
+
+    console.log('el sueldo anual del empleado es : ' + multiplicacion );
